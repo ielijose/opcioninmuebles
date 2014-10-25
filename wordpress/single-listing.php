8380f3e1b2434088f8280ce3f78fe55c7a623ba4
Eli José Carrasquero [PC] <ielijose@gmail.com>
2014-10-25 01:35:52 -0430

    Archivos de wordpress modificados

---
+++ wordpress/single-listing.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * FILE: single-listing.php 
+ * Created on Aug 8, 2013 at 4:27:57 PM 
+ * Author: Mr.Vibe 
+ * Credits: www.VibeThemes.com
+ * Project: vEstate 
+ * License: GPLv2
+ */
+
+
+global $vibe_options,$post;
+$vibe_options = get_option(THEME_SHORT_NAME);
+get_header($vibe_options['header_style']);
+
+$rand='subheader'.rand(1,999);
+
+$show=array();
+$show = get_show_values();
+$post_type=  get_post_type();
+$custom_subheader =get_subheader_values();
+$custom_css = get_custom_css();
+
+$inpage_menu=vibe_inpagemenu();
+
+?>
+<style>
+<?php
+    if(isset($vibe_options['custom_css_changes']) && in_array($post_type,$vibe_options['custom_css_changes'])){
+       $custom_css = get_custom_css();  
+    echo 'header{
+        display: '.(($custom_css['header'])?'block':'none').';
+    }';
+    if(isset($custom_css['body_bg_image']) && strlen($custom_css['body_bg_image']) > 6 ){
+    echo 'body{
+        background-image: '.$custom_css['body_bg_image'].';
+        '.$custom_css['body_css'].'
+    }';
+    }
+    
+    if(isset($custom_css['general_css']) && $custom_css['general_css'])
+    echo $custom_css['general_css'];
+    
+    if(isset($custom_css['footer']) && $custom_css['footer']){
+    echo 'footer{
+         display: '.(($custom_css['footer'])?'block':'none').';
+        }';
+        }
+    }
+    
+
+    if(isset($vibe_options['subheader_css_changes']) && in_array($post_type,$vibe_options['subheader_css_changes'])){
+       echo '#'.$rand.', .boxed #'.$rand.' .container{
+          '.(isset($custom_subheader['subheader_bg_color'])?'background-color:'.$custom_subheader['subheader_bg_color'].';':'').'
+         '.((isset($custom_subheader['subheader_bg_image']) && strlen($custom_subheader['subheader_bg_image']) > 6)?'background-image: '.$custom_subheader['subheader_bg_image'].'':'').
+            ''.$custom_subheader['subheader_css'].' }';
+    
+    
+    echo  '#'.$rand.' ul.breadcrumbs,
+         #'.$rand.' h1,
+         #'.$rand.' h5,
+         #'.$rand.' a{color:'.$custom_subheader['subheader_color'].';}';
+    }
+   
+    ?>
+</style>
+<?php
+    if($show['subheader']){
+?>
+<section id="<?php echo $rand; ?>" class="subheader <?php echo $vibe_options['header_style']; ?>">
+    <div class="container">
+        <div class="row">
+            <div class="<?php echo $show['headingspan']; ?>">
+                <?php if($show['title']){ ?>
+               <h1 id="page_title"><?php the_title(); ?></h1> 
+               <?php 
+                        }
+                     if(strlen($custom_subheader['subtitle'])>2){
+                         $subtitle=html_entity_decode($custom_subheader['subtitle']);
+                        if(!(strcmp( $subtitle, strip_tags($subtitle) ) == 0)){
+                            echo $subtitle;
+                        }else {
+                          echo '<h5>'.$custom_subheader['subtitle'].'</h5>';  
+                        }
+                    }else{
+                        if(isset($vibe_options['primary_listing_parameter']))
+                            echo '<h5>'.get_the_term_list( $post->ID, $vibe_options['primary_listing_parameter'], ' ', ', ', '' ).'</h5>';
+                    }
+                 if($show['prev_next']){ 
+                            ?>
+                            <div class="prev_next_links">
+                                <div class="prev_next">
+                                <?php 
+                                previous_post_link('%link', '<i class="icon-left-open-mini"></i>');
+                                next_post_link('%link', '<i class="icon-right-open-mini"></i>');
+                                ?>
+                                </div>    
+                            </div>
+                            <?php
+                            }
+                            ?>
+            </div>
+            <?php
+             if($show['breadcrumbs']){
+            ?>
+            <div class="span4">
+                    <?php 
+                       if (function_exists('vibe_breadcrumbs')) 
+                        vibe_breadcrumbs(); 
+                       ?>   
+            </div>
+        </div>      
+    </div>
+    <?php
+                } //Hide Show BreadCrumbs
+                if($inpage_menu){
+                    $inclass='';
+                    if(isset($vibe_options['nav_fix']) && !$vibe_options['nav_fix'])
+                      $inclass='fixtop';
+                    
+            ?>
+           <div class="inpage_menu <?php echo $inclass; ?>">
+               <div class="container">
+                   <div class="row">
+                        <div class="span12">
+                            <ul class="in_menu">
+                            <?php
+                                    foreach($inpage_menu as $key=>$item){
+                                    echo '<li><a href="#'.$key.'">'.$item.'</a></li>';
+                                    }
+                                ?>
+                            </ul>
+                        </div> 
+                    </div>    
+                   </div>    
+               </div>    
+            <?php
+                }
+            ?>
+</section> 
+<?php
+}
+?>
+<section class="main">
+	<div class="container">
+            <div class="row">
+               <?php
+                        
+                        get_template_part( 'listing', $show['template']);
+                ?>   
+            </div>
+	</div>
+</section>   
+<script>    
+    var url = "//localhost:8001";
+
+    <?php echo "var id = '" . getPostMeta($post->ID,'vibe_property-id')." ';"; ?>
+    
+    jQuery.get(url + '/api/statistic/' + id + '/visit', function(data) {
+    }, 'json');
+</script>
+
+<?php
+get_footer();
+?>
+
