124307be593af8ab7d850c42c7d7de745200da67
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-02-19 18:04:22 -0430

    fix branch line 92

--- app/controllers/BranchController.php
+++ app/controllers/BranchController.php
@@ -89,7 +89,7 @@ class BranchController extends BaseController {
 		$portals = Portal::all();
 		$services = Service::all();
 
-		$branch = branch::findOrFail($id);
+		$branch = Branch::findOrFail($id);
 		return View::make('backend.branches.edit', compact('branch', 'categories', 'cities', 'portals', 'services'));
 	}
 
